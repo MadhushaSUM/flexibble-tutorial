56768f56d332e4f296119cd43a03ef521cb606e6
Madhusha Laksitha <madhusha.sum@gmail.com>
2023-08-26 17:42:57 +0530

    Develop dummy homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 const Home = () => {
     return (
-        <h1>Home</h1>
+        <section className="flex-start flex0col paddings mb-16">
+            <h1>Categories</h1> 
+            <h1>Posts</h1>
+            <h1>Load more</h1>
+        </section>
     )
 }
 
