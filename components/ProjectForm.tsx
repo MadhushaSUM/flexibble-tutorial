7fc4c9d32cf98d94cf4fc99187c488e65235acfc
Madhusha Laksitha <madhusha.sum@gmail.com>
2023-08-31 21:10:33 +0530

    Develop create-project page

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -2,6 +2,11 @@
 
 import { SessionInterface } from "@/common.types"
 import Image from "next/image"
+import FormField from "./FormField";
+import { categoryFilters } from "@/constants";
+import CustomMenu from "./CustomMenu";
+import { ChangeEvent, useState } from "react";
+import Button from "./Button";
 
 type Props = {
     type: string,
@@ -11,12 +16,39 @@ type Props = {
 const ProjectForm = ({ type, session }: Props) => {
 
     const handleFormSubmit = (e: React.FormEvent) => {};
-    const handleChangeImage = (e: React.FormEvent) => {};
+    const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
 
-    const form = {
-        image:"",
+        const file = e.target.files?.[0];
+        if (!file) return;
+
+        if (!file.type.includes("image")) {
+            return alert("Please upload an image file");
+        }
+
+        const reader = new FileReader();
+
+        reader.readAsDataURL(file);
+
+        reader.onload = () => {
+            const result = reader.result as string;
+
+            handleStateChange("image", result);
+        }
+    };
+    const handleStateChange = (fieldName: string, value: string) => {
+        setForm((prevState) => ({ ...prevState, [fieldName]: value }))
     };
 
+    const [submitting, setSubmitting] = useState(false);
+    const [form, setForm] = useState({
+        title:"",
+        description:"",
+        image:"",
+        liveSiteUrl:"",
+        githubUrl:"",
+        category:"",
+    });
 
     return (
         <form onSubmit={handleFormSubmit} className="flexStart form">
@@ -42,6 +74,53 @@ const ProjectForm = ({ type, session }: Props) => {
                         fill
                     />
                 )}
+            </div>
+
+            <FormField 
+                title="Title"
+                state={form.title}
+                placeholder="Flexible"
+                setState={(value) => handleStateChange("title", value)}
+            />
+            <FormField 
+                title="Description"
+                state={form.description}
+                placeholder="Showcase and discover remarkable developer projects"
+                setState={(value) => handleStateChange("description", value)}
+            />
+            <FormField 
+                type="url"
+                title="Website URL"
+                state={form.liveSiteUrl}
+                placeholder="https://www.sum.lk"
+                setState={(value) => handleStateChange("liveSiteUrl", value)}
+            />
+            <FormField 
+                type="url"
+                title="Github URL"
+                state={form.githubUrl}
+                placeholder="https://github.com/MadhushaSUM"
+                setState={(value) => handleStateChange("githubUrl", value)}
+            />
+
+            <CustomMenu 
+                title="Category"
+                state={form.category}
+                filters={categoryFilters}
+                setState={(value) => handleStateChange("category", value)}
+            />
+
+            <div className="flexStart w-full">
+                <Button
+                    title={
+                        submitting
+                        ? `${type === "create" ? "Creating" : "Editing"}`
+                        : `${type === "create" ? "Create" : "Edit"}`
+                    }
+                    type="submit"
+                    leftIcon={submitting ? "" : "/plus.svg"}
+                    isSubmitting={submitting}
+                />
 
             </div>
 
